029335f3c274b9b09935c181747b1643a488a525
ArielPrats <ar.prats@duocuc.cl>
2025-05-09 12:11:23 -0400

    commit

--- print(Five Nights at freddys en python).py
+++ print(Five Nights at freddys en python).py
@@ -32,56 +32,52 @@ noche=int(input())
 
 while noche>7:
     print("error,no se reconoce noche")
-    noche=int(input())
+    noche=(input())
 
-AI=int
-AI2=int
-AI3=int
-AI4=int
 
 pila=int 
 pila=100
 
 while noche==1:
-    AI==0
-    AI2==3
-    AI3==2
-    AI4==2
+    AI=0
+    AI2=3
+    AI3=2
+    AI4=2
     noche=10
 
 while noche==2:
-    AI==0
-    AI2==6
-    AI3==3
-    AI4==3
+    AI=0
+    AI2=6
+    AI3=3
+    AI4=3
     noche=20
 
 while noche==3:
-    AI==1
-    AI2==3
-    AI3==7
-    AI4==4
+    AI=1
+    AI2=3
+    AI3=7
+    AI4=4
     noche=30
 
 while noche==4:
-    AI==random.randint(1,20)
-    AI2==5
-    AI3==6
-    AI4==8
+    AI=random.randint(1,20)
+    AI2=5
+    AI3=6
+    AI4=8
     noche=40
 
 while noche==5:
-    AI==3
-    AI2==8
-    AI3==9
-    AI4==7
+    AI=3
+    AI2=8
+    AI3=9
+    AI4=7
     noche=50
 
 while noche==6:
-    AI==4
-    AI2==13
-    AI3==14
-    AI4==18
+    AI=4
+    AI2=13
+    AI3=14
+    AI4=18
     noche=60
 
 while noche==7:
@@ -166,14 +162,14 @@ while noche==70:
 print("12:00AM")
 print (" ")
 
-XD=int
-mov1=int
-mov2=int
-mov3=int
-mov4=int
-num=int
+XD=0
+mov1=0
+mov2=0
+mov3=0
+mov4=0
+num=0
 
-while XD==60 or mov1==17 or mov2==15 or mov3==13 or mov4==11:
+while XD!=60 or mov1!=17 or mov2!=15 or mov3!=13 or mov4!=11:
     
     while XD==1:
         print("12:00 AM")
@@ -199,9 +195,9 @@ while XD==60 or mov1==17 or mov2==15 or mov3==13 or mov4==11:
         print("5:00AM")
         XD=XD+1
 
+    XD=0
     XD=XD+1
 
-    pila=int
     cam=int
 
     if pila<1:
@@ -346,4 +342,208 @@ while XD==60 or mov1==17 or mov2==15 or mov3==13 or mov4==11:
         print("BONNIE esta en la puerta")
         mov2=mov2+1
 
+   
+    num3=random.randint(1,20)
+
+    if AI3==num3 or AI3>num3:
+        mov3=mov3+1
+    else:
+        print("-CHICA")
+
+    while mov3==1 and cam==2:
+        print("-CHICA")
+        mov3=mov3+1
+
+    while mov3==1 and cam==1:
+        print("CHICA esta en el esenario")
+        mov3=mov3+1
+
+    while mov3==3 and cam==2:
+        print("-CHICA")
+        mov3=mov3+1
+
+    while mov3==3 and cam==1:
+        print("CHICA esta en el comedor")
+        mov3=mov3+1
+
+    while mov3==5 and cam==2:
+        print("-CHICA")
+        mov3=mov3+1
+
+    while mov3==5 and cam==1:
+        print("CHICA esta en la cocina")
+        mov3=mov3+1
+
+    while mov3==7 and cam==2:
+        print("-CHICA")
+        mov3=mov3+1
+
+    while mov3==7 and cam==1:
+        print("CHICA esta caminando al pasillo")
+        mov3=mov3+1
+
+    while mov3==9 and cam==2:
+        print("-CHICA")
+        mov3=mov3+1
+
+    while mov3==9 and cam==1:
+        print("CHICA esta en el pasillo")
+        mov3=mov3+1
+
+    while mov3==11 and cam==2:
+        print("-CHICA")
+        mov3=mov3+1
+
+    while mov3==11 and cam==1:
+        print("CHICA esta en la puerta")
+        mov3=mov3+1
+
     
+    num4=random.randint(1,20)
+    
+    if AI4==num4 or AI4>num4:
+        mov4=mov4+1
+    else:
+        print("-FOXY")
+
+    while mov4==1 and cam==2:
+        print("-FOXY")
+        mov4=mov4+1
+
+    while mov4==1 and cam==1:
+        print("FOXY esta en la pirate cover")
+        mov4=mov4+1
+
+    while mov4==3 and cam==2:
+        print("-FOXY")
+        mov4=mov4+1
+
+    while mov4==3 and cam==1:
+        print("FOXY esta asomando por la cortina")
+        mov4=mov4+1
+
+    while mov4==5 and cam==2:
+        print("-FOXY")
+        mov4=mov4+1
+
+    while mov4==5 and cam==1:
+        print("FOXY esta saliendo de su cueva")
+        mov4=mov4+1
+
+    while mov4==7 and cam==2:
+        print("-FOXY")
+        mov4=mov4+1
+
+    while mov4==7 and cam==1:
+        print("FOXY esta fuera de su pirate cover")
+        mov4=mov4+1
+
+    while mov4==9 and cam==2:
+        print("-FOXY")
+        mov4=mov4+1
+
+    while mov4==9 and cam==1:
+        print("FOXY esta corriendo a la oficina")
+        mov4=mov4+1
+
+    print("quieres ver las camaras? si(1) o no(2)")
+    cam=int(input())
+
+    if cam==1:
+        pila=pila-1
+        print("cam system on")
+    else:
+        print("cam system off")
+
+    while cam>2 or cam==0:
+        print("error, comando de camara denegado")
+        cam=int(input())
+
+    print("quieres cerrar(1) o abrir(2) la puerta?")
+    puerta=int(input())
+
+    while puerta>2 or puerta==0:
+        print("error, comando de puerta denegado")
+        puerta=int(input())
+
+    if puerta==1:
+        pila=pila-1
+        print("puerta cerrada")
+    else:
+        puerta=2 
+        print("puerta abierta")
+
+    while mov1==16 and puerta==1:
+        mov1=1
+        pila=pila-1
+        print("-FREDDY vuelve al esenario")
+
+    while mov1==16 and puerta==2:
+        print("FREDDY a entrado a la oficina")
+        mov1=17
+
+    while mov2==14 and puerta==1:
+        mov2=1
+        pila=pila-1
+        print("-BONNIE vuelve al esenario")
+
+    while mov2==14 and puerta==2:
+        print("BONNIE a entrado a la oficina")
+        mov2=15
+
+    while mov3==12 and puerta==1:
+        mov3=1
+        pila=pila-1
+        print("-CHICA vuelve al esenario")
+
+    while mov3==12 and puerta==2:
+        print("CHICA a entrado a la oficina")
+        mov3=13
+
+    while mov4==10 and puerta==1:
+        mov4=1
+        pila=pila-1
+        print("-FOXY vuelve al pirate cover")
+
+    while mov4==10 and puerta==2:
+        print("FOXY a entrado a la oficina")
+        mov4=11
+        pila=pila-1
+
+    golden=0
+    golden2=0
+    mov=0
+
+    golden=random.randint(1,50000)
+    golden2=random.randint(1,50000)
+
+    while golden==golden2:
+        print('''
+            "          0 0 0 0 0 0          "
+			"          0         0          "
+			"0 0 0     0=========0     0 0 0"
+			"0   0   0 0 0 0 0 0 0 0   0   0"
+			"0 0 0 0                 0 0 0 0" 
+			"    0   0 0 0     0 0 0   0    " 
+			"    0   0 o 0     0 o 0   0    "
+			"    0   0 0_       _0 0   0    " 
+		    "    0       0 0 0 0       0    "
+			"  0     0 0         0 0     0  "
+			"  0   0     0 0 0 0     0   0  "
+			"  0   0                 0   0  "
+			"    0   0 0 0 0 0 0 0 0   0    " 
+			"    0 =================== 0    " 
+			"    0 =================== 0    " 
+			"      0 0 0 0 0 0 0 0 0 0      " ''')
+        golden=2
+        golden2=1
+        mov=9
+
+    
+
+if XD==60:
+    print("6:00AM, Noche Completada!")
+else:
+    print("GAME OVER")
+    print("turnos sobrevividos")
+    print(XD)
