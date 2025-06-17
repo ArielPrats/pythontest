5f2530e2bf41d36944a77fe8d431e4f66a601d0f
ArielPrats <ar.prats@duocuc.cl>
2025-06-17 12:05:55 -0400

    Add files via upload

---
+++ #IA DE SPRINTRAP PROTOTITPO.py
@@ -0,0 +1,20 @@
+#IA DE SPRINTRAP PROTOTITPO
+import random 
+
+hola=4
+totalturns=0
+agresive=0
+
+while hola!=1:
+    totalturns=totalturns+1
+    AI=random(1,3) 
+    if AI==2 or 3:    
+        mov=mov+1
+    else:
+        agresive=agresive+1
+        totalturns=totalturns+1
+
+    10-AI-agresive+random(1,15)-totalturns
+
+    
+
